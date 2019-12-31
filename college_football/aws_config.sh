04fce2f1fb2fa07d37f7d457c0b92306b3b3fb8a
b-shelton <49220496+b-shelton@users.noreply.github.com>
2019-12-31 15:40:48 -0500

    Update aws_config.sh

--- college_football/aws_config.sh
+++ college_football/aws_config.sh
@@ -12,6 +12,7 @@ python3 -m pip install pandas
 python3 -m pip install requests
 python3 -m pip install bs4
 python3 -m pip install s3fs
+python3 -m pip install matplotlib
 
 # check drive usage
 df -H --output=source,size,used,avail
